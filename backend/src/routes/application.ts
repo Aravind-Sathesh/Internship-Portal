a9ac2f4a4661929b6979c8101b9cea036bc2cf46
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-11-01 15:26:12 +0530

    refactor: update applicationController to be more versatile
    - combine acceptApplication and withdrawApplication into updateApplication
    - remove unused deleteApplication and getAllApplications

--- backend/src/routes/application.ts
+++ backend/src/routes/application.ts
@@ -1,12 +1,8 @@
 import express from 'express';
 import {
 	createApplication,
-	getAllApplications,
 	getApplicationById,
 	updateApplication,
-	deleteApplication,
-	withdrawApplication,
-	acceptApplication,
 	getApplicationsByStudentId,
 	getApplicationsByEmployerId,
 } from '../controllers/applicationController';
@@ -14,13 +10,9 @@ import {
 const router = express.Router();
 
 router.post('/', createApplication);
-router.get('/', getAllApplications);
 router.get('/:id', getApplicationById);
+router.get('/student/:studentId', getApplicationsByStudentId);
 router.get('/employer/:employerId', getApplicationsByEmployerId);
 router.put('/:id', updateApplication);
-router.delete('/:id', deleteApplication);
-router.get('/student/:studentId', getApplicationsByStudentId);
-router.post('/:id/cancel', withdrawApplication);
-router.post('/:id/accept', acceptApplication);
 
 export default router;
