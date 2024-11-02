a0990e77b7d4b39198e9060122e5bee388bbfbab
Aravind Sathesh <aravind.sathesh@gmail.com>
2024-11-02 15:19:30 +0530

    feat: implement email notifications for password reset and application updates using nodemailer

--- backend/src/routes/employer.ts
+++ backend/src/routes/employer.ts
@@ -4,6 +4,8 @@ import {
 	updateProfile,
 	deleteProfile,
 	createProfile,
+	resetPassword,
+	sendPasswordResetEmail,
 } from '../controllers/employerController';
 import { authenticateToken } from '../middleware/auth';
 
@@ -13,6 +15,8 @@ router.post('/login', login);
 router.put('/update-profile/:id', updateProfile);
 router.post('/register', createProfile);
 router.delete('/delete-profile/:id', deleteProfile);
+router.post('/send-password-reset-email', sendPasswordResetEmail);
+router.post('/reset-password', resetPassword);
 router.get('/profile', authenticateToken, (req, res) => {
 	res.json(req.employer);
 });
